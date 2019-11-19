c927b341cd2ebdd52baa0f6eca7ee9a7bd323a25
medram <mramouchy@gmail.com>
2019-11-20 00:08:15 +0100

    show all new movies

--- bots/YTSThread.py
+++ bots/YTSThread.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import glob
 import requests
 import plyer
 
@@ -24,45 +25,75 @@ class YTSThread(BotThread):
 	def run(self):
 		self.show('is running...', force=True)
 		while not self.stopped():
-			post = self._getLastMovie()
-			if post:
-				if self._checkSaveNewMovie(post):
-					self.show('New movie has been added.')
-					self._notifyMe(post)
-				else:
-					self.show('Nothing new.')
+			posts = self._getLastMovies()
+			if posts:
+				self.show('New movies have been posted.')
+				self._notifyMe(posts)
+			else:
+				self.show('Nothing new.')
 
 			#raise KeyboardInterrupt('Stop this thread')
 			#time.sleep(self._sleep)
 			self._stop.wait(self._sleep)
 		self.show('was stopped.', force=True)
 
-	def _getLastMovie(self):
-		post = {}
+	def _getLastMovies(self):
+		
+		posts = []
 		try:
 			#jar = requests.cookies.RequestsCookieJar()
 			res = requests.get(self._url, timeout=(5, 30), cookies=self._cookies)
 			soup = BeautifulSoup(res.text, 'lxml')
 
-			box = soup.find('div', class_='browse-movie-wrap')
-			title = box.find('a', class_='browse-movie-title').text.strip()
-			released = box.find('div', class_='browse-movie-year').text.strip()
-			cover = box.find('img').attrs.get('src').strip()
-			link = box.find('a', class_='browse-movie-title').attrs.get('href').strip()
-			try:
-				availableIn = [child.text.strip() for child in box.find('div', class_='browse-movie-tags').children if child.name == 'a']
-			except AttributeError:
-				availableIn = '--'
+			boxes = soup.findAll('div', class_='browse-movie-wrap')
+			
+			for box in boxes:
+				title = box.find('a', class_='browse-movie-title').text.strip()
+				released = box.find('div', class_='browse-movie-year').text.strip()
+				cover = box.find('img').attrs.get('src').strip()
+				link = box.find('a', class_='browse-movie-title').attrs.get('href').strip()
+				try:
+					availableIn = [child.text.strip() for child in box.find('div', class_='browse-movie-tags').children if child.name == 'a']
+				except AttributeError:
+					availableIn = '--'
 
-			# fill in the post.
-			post['title'] = title
-			post['link'] = link
-			post['released'] = released
-			post['cover'] = cover
-			post['availableIn'] = availableIn
-			post['downloaded_cover'] = ''
+				post = {}
+				# fill in the post.
+				post['title'] = title
+				post['link'] = link
+				post['released'] = released
+				post['cover'] = cover
+				post['availableIn'] = availableIn
+				post['downloaded_cover'] = ''
 
-			return post
+				posts.append(post)
+
+			# filter the newest movies.
+			for i, post in enumerate(posts):
+				if not self._checkSaveNewMovie(post):
+					del posts[i:]
+					break
+
+			if posts:
+				# clean up old covers
+				for file in glob.glob(f'images/{self.getName()}_*'):
+					try:
+						os.remove(file)
+					except IsADirectoryError:
+						pass
+
+				if len(posts) > 15:
+					posts = posts[0:4]
+
+				# download new covers
+				for post in posts:
+					# download new movie cover
+					post['downloaded_cover'] = download(post['cover'], rename_to=f"{self.getName()}_{post['title']}")
+
+				# save the lastest post.
+				self._checkSaveNewMovie(posts[0], save=True)
+
+			return posts
 
 		except ConnectionError:
 			self.show('Connection failed: Please check your internet connection.')
@@ -75,37 +106,37 @@ class YTSThread(BotThread):
 
 		return False
 
-	def _checkSaveNewMovie(self, post:dict):
+	def _checkSaveNewMovie(self, post:list, save=False):
 		post_from_json = self._config.get('last_post')
 
 		if post_from_json is None or post_from_json['link'] != post['link']:
-			data = self._config.get()
-			# download new movie cover
-			post['downloaded_cover'] = download(post['cover'], rename_to=self.getName())
-			
-			data['last_post'] = post
-			self._config.save(data)
+			if save:
+				data = self._config.get()
+				# download new movie cover
+				#post['downloaded_cover'] = download(post['cover'], rename_to=f"{self.getName()}_{post['title']}")
+				data['last_post'] = post
+				self._config.save(data)
 			return True
 		return False
 
-	def _notifyMe(self, post:dict):
+	def _notifyMe(self, posts:dict):		
+		for post in posts:
+			cover = os.path.abspath(post['downloaded_cover'])
+			
+			if sys.platform == 'win32':
+				cover = self._convertToICO(cover)
 
-		cover = os.path.abspath(post['downloaded_cover'])
-		
-		if sys.platform == 'win32':
-			cover = self._convertToICO(cover)
-
-		try:
-			plyer.notification.notify(
-					title=f"[YTS] {post.get('title')}.",
-					message=f"Released in: {post.get('released')}\nAvailable in: {', '.join(post.get('availableIn'))}",
-					timeout=self._notif_timeout,
-					app_name=self.getName(),
-					app_icon=cover
-					#ticker=True
-				)
-		except NotImplementedError as e:
-			self.show(e, force=True)
+			try:
+				plyer.notification.notify(
+						title=f"[YTS] {post.get('title')}.",
+						message=f"Released in: {post.get('released')}\nAvailable in: {', '.join(post.get('availableIn'))}",
+						timeout=self._notif_timeout,
+						app_name=self.getName(),
+						app_icon=cover
+						#ticker=True
+					)
+			except NotImplementedError as e:
+				self.show(e, force=True)
 
 	def _convertToICO(self, path):
 		try:
